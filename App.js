aae2b41f7dad9638cc39544d2b98f2761adeceb5
kouci747 <koceila.arhab@gmail.com>
2023-03-07 12:22:22 +0100

    Remade the todo myself

--- App.js
+++ App.js
@@ -1,63 +1,74 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, Image, ScrollView} from 'react-native';
-import axios from 'axios';
-import Counter from './src/components/counter';
-// import Home from './src/screens/home';
-// import Practice from './src/screens/practice';
-// import Trombinoscope from './src/screens/trombinoscope';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Button,
+} from 'react-native';
 
-const App = () => {
-  const [data, setData] = useState([]); //car la data dans l'API se présente sous forme de tableau
-
-  useEffect(() => {
-    axios
-      .get('https://hp-api.onrender.com/api/characters/students')
-      .then(res => {
-        setData(res.data);
-        // console.log(res.data);
-      });
-  });
-
-  return (
-    <>
-      <Counter />
-      <View>
-        <Text style={styles.text}>Le trombinoscope d'Harry Potter</Text>
-        <ScrollView style={styles.scroll}>
-          {data?.map(item => (
-            <View key={item.id}>
-              <Text style={styles.container}>{item.name}</Text>
-              <Text>{item.house}</Text>
-              <Image
-                source={{
-                  uri: item.image
-                    ? item.image
-                    : 'https://www.tradeinn.com/f/13888/138888580/warner-bros-coussin-harry-3d.jpg',
-                }}
-                style={styles.image}
-              />
-            </View>
-          ))}
-        </ScrollView>
-      </View>
-    </>
-  );
-};
+//styles
 const styles = StyleSheet.create({
-  container: {color: 'red'},
-
-  image: {
-    width: 80,
-    height: 80,
-  },
-  scroll: {
-    height: 700,
-  },
-  text: {
-    color: 'green',
-    fontSize: 20,
-    justifyContent: 'center',
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
   },
 });
 
-export default App;
+const MyToDoList = () => {
+  const [toDos, setToDos] = useState([]);
+  const [text, setText] = useState('');
+
+  //ajouter dans la todolist
+  const handleAddToList = () => {
+    //4 : to add the latest value to the list from step 3,
+    //the setToDos takes all previous values in it with ...toDos + adds the latest 'text' value
+    if (text.length >= 3) {
+      setToDos([...toDos, text]);
+      //5 : setText field is blanked
+      setText('');
+    } else {
+      alert('longeur minimale non atteinte');
+    }
+  };
+  console.log('here are the ', toDos);
+  //supprimer de la todolist
+  const handleDeleteFromList = index => {
+    const allToDos = [...toDos]; //fetch all toDos in the list and put them in an array
+    //we do that because we should not mutate the original state directly
+    allToDos.splice(index, 1); //remove the todo item with the index of 1
+    setToDos(allToDos); //reset the todo list of items without the deleted value
+  };
+  return (
+    <View>
+      <Text> TO DO LIST</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="entrez quelque chose à faire ici"
+        value={text} //1: catch the written value
+        onChangeText={value => setText(value)}></TextInput>
+      {/*2 :setting the text with the latest captured value */}
+      <TouchableOpacity onPress={handleAddToList}>
+        {/* 3 : when the button is clicked, handleAddToList is called */}
+        <Text>Add Todo</Text>
+      </TouchableOpacity>
+
+      {toDos.map((todo, index) => (
+        <TouchableOpacity key={index} onPress={handleDeleteFromList}>
+          <Text>{todo}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+export default function App() {
+  return (
+    <View>
+      <MyToDoList />
+    </View>
+  );
+}
